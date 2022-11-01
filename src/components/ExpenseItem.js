664e8826e4df92d33b82d93045171a4fe910bb07
lalithcodelover <lalith.vlr@gmail.com>
2022-11-01 13:48:15 +0530

    passing data with props and looping added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022,11,23);
-  const expenseTitle = "Car insurance";
-  const expenseLocation = "Showroom";
-  const expenseAmount = 294.95;
-  
+function ExpenseItem(props) {
+
   return (
     <div className='expense-item'>
-     <div>{expenseDate.toISOString()}</div>
+     <div>{props.date.toISOString()}</div>
      <div className='expense-item__description'>
-      <h2>{expenseTitle}</h2>
-      <h2>{expenseLocation}</h2>
-      <div className='expense-item__price'>${expenseAmount}</div>
+      <h2>{props.title}</h2>
+      <h2>{props.location}</h2>
+      <div className='expense-item__price'>${props.amount}</div>
      </div>
     </div>
   );
