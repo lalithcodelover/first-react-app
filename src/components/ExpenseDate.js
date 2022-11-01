ab3b278c773c9906f0e5cd1a6207a9b910c45602
lalithcodelover <lalith.vlr@gmail.com>
2022-11-01 15:43:12 +0530

    child components of expense item added

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css'
+
+function ExpenseDate(props) {
+  const day = props.date.getDate();
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
-import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
   return (
-    <div className='expense-item'>
-     <div>{props.date.toISOString()}</div>
-     <div className='expense-item__description'>
-      <h2>{props.title}</h2>
-      <h2>{props.location}</h2>
-      <div className='expense-item__price'>${props.amount}</div>
-     </div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        amount={props.amount}
+        location={props.location}
+      />
     </div>
   );
 }
