70de89d1b967566d2be8ec5e9bf5462f3291720f
lalithcodelover <lalith.vlr@gmail.com>
2022-11-02 22:58:49 +0530

    states for input added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,35 @@
+import { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [enteredTitle,setTitle] = useState()
+    const [enteredAmount,setAmount] = useState()
+    const [enteredDate,setDate] = useState()
+
+    const addTitle = (e) => {
+        setTitle(e.target.value)
+        
+    }
+    const addAmount = (e) => {
+        setAmount(e.target.value)
+    }
+    const addDate = (e) => {
+        setDate(e.target.value)
+    }
 
     return <form>
         <div className='.new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type="text" />
+                <input type="text" onChange={addTitle}/>
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01"/>
+                <input type="number" min="0.01" step="0.01" onChange={addAmount}/>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31"/>
+                <input type="date" min="2019-01-01" max="2022-12-31" onChange={addDate}/>
             </div>
         </div>
         <div className='new-expense__actions'>
