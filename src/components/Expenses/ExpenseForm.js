a9e29f63bddb9c51a15f9ca2f4c6068590df7995
lalithcodelover <lalith.vlr@gmail.com>
2022-11-02 22:35:15 +0530

    log input added

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,14 +1,24 @@
 
 const ExpenseForm = ()=>{
 
+    const addTitle = (e) =>{
+         console.log(e.target.value)
+    }
+    const addAmount = (e) =>{
+        console.log(e.target.value)
+   }
+   const addDate = (e) =>{
+    console.log(e.target.value)
+}
     return (
     <div>
+        <br />
         <label >Expense Title</label>
-        <input type="text" />
+        <input type="text" onChange={addTitle} />
         <label >Expense Amount</label>
-        <input type="number" />
+        <input type="number" onChange={addAmount}/>
         <label >Expense Date</label>
-        <input type="date"  />
+        <input type="date"  onChange={addDate}/>
         <button>Add Expense</button>
     </div>
     )
