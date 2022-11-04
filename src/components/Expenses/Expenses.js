2cac3ff61edc02a7e900c7c7d0a3a0a985d9bfb5
lalithcodelover <lalith.vlr@gmail.com>
2022-11-04 15:21:58 +0530

    filter by year added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,28 +6,34 @@ import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
-    
   };
 
   return (
     <div>
       <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => {
-          return (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-              location={expense.location}
-            ></ExpenseItem>
-          );
-        })}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items
+          .filter((expense) => {
+            return expense.date.getFullYear().toString() === filteredYear;
+          })
+          .map((expense) => {
+            return (
+              <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                location={expense.location}
+              ></ExpenseItem>
+            );
+          })}
       </Card>
     </div>
   );
