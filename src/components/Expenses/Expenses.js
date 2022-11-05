80593ebd1de9b7264892e0241acc7211a719815c
lalithcodelover <lalith.vlr@gmail.com>
2022-11-05 21:27:11 +0530

    Chart bars added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,20 +24,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpense.length===0 && <p>No expense to show.</p>}
-        {filteredExpense.length>0 && filteredExpense.map((expense) => {
-            return (
-              <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-                location={expense.location}
-              ></ExpenseItem>
-            );
-          })}
-          {filteredExpense.length===1 && <p>Only single expense here. Please add MORE!!!</p>}
-
+        <ExpensesChart expenses={filteredExpense} />
+        <ExpensesList items={filteredExpense}/>
       </Card>
     </div>
   );
